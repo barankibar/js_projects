15a35ccbdee0943b16257475b369f11a7166a9b6
baran <baran@DESKTOP-CRRNS6C>
2022-04-23 15:22:27 +0300

    hamburger-button

--- js/hamburger-button.js
+++ js/hamburger-button.js
@@ -5,9 +5,13 @@ let menu = document.getElementById("menu");
 button.onclick = function(){
     if(sideNav.style.right == "0px"){
         sideNav.style.right = "-300px"
+        button.style.right = "100px"
+        menu.src = "/img/hamburger-button.png"
     }
     else{
         sideNav.style.right = "0px"
+        button.style.right = "300px"
+        menu.src = "/img/close.png"
     }
     menu.style.transitionDuration = "1s";
 }
