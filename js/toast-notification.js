f5c04a78c829c74f5e6d47555e7aa82667e79214
baran <baran@DESKTOP-CRRNS6C>
2022-04-24 21:05:21 +0300

    toast-notification

---
+++ js/toast-notification.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById("btn");
+const container = document.getElementById("container");
+
+btn.addEventListener('click', () => {
+    createNotification();
+});
+
+function createNotification(){
+    const notif = document.createElement("div");
+    notif.classList.add("toast");
+    
+    notif.innerText = "Thanks For Click";
+    
+    container.appendChild(notif);
+
+    setInterval(() => {
+        notif.remove();
+    }, 3000);
+};
